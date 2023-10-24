bc7f15e6285da71703adfb42b16ee3cf9f1ef328
Amizadayor <Juanamizadayo@gmail.com>
2023-10-24 14:06:09 -0600

    Controlador-Modelo-Migración 0.0.07

---
+++ app/Models/Municipio.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Municipio extends Model
+{
+    use HasFactory;
+    protected $table = 'municipios';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'NombreMunicipio',
+        'Disid'
+    ];
+    public $timestamps = true;
+
+    public function Distrito()
+    {
+        return $this->belongsTo(Distrito::class, 'Disid', 'id');
+    }
+}
