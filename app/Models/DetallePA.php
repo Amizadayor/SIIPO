876cec751ebe6bdd3fbe3be6c16ca9772f44f0cc
Amizadayor <Juanamizadayo@gmail.com>
2023-10-25 12:28:31 -0600

    Controlador-Modelo-Migración 0.0.18

---
+++ app/Models/DetallePA.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\DatosGeneralesPA;
+
+class DetallePA extends Model
+{
+    use HasFactory;
+    protected $table = 'detalles_pa';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'IniOperaciones',
+        'ActvPesqueraAcuacultura',
+        'ActvPesqueraCaptura',
+        'ActivoEmbMayor',
+        'ActivoEmbMenor',
+        'DocActaNacimiento',
+        'DocComprobanteDomicilio',
+        'DocCURP',
+        'DocIdentificacionOfc',
+        'DocRFC',
+        'DGPAid',
+    ];
+    public $timestamps = true;
+    public function DatosGeneralesPA()
+    {
+        return $this->belongsTo(DatosGeneralesPA::class, 'DGPAid', 'id');
+    }
+}
