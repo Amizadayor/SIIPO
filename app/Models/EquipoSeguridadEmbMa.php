c55bd4f8641bc8b20416a6185dda2312573f10cd
Amizadayor <Juanamizadayo@gmail.com>
2023-10-26 11:08:54 -0600

    Controlador-Modelo-Migración 0.0.40

---
+++ app/Models/EquipoSeguridadEmbMa.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\EquipoSeguridad;
+use App\Models\DatosGeneralesEmbMa;
+
+class EquipoSeguridadEmbMa extends Model
+{
+    use HasFactory;
+    protected $table = 'equipos_seguridad_emb_ma';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'EqpoSeguridadid',
+        'DGEMMAid',
+    ];
+    public $timestamps = true;
+
+    public function EquipoSeguridad()
+    {
+        return $this->belongsTo(EquipoSeguridad::class, 'EqpoSeguridadid', 'id');
+    }
+
+    public function DatosGeneralesEmbMa()
+    {
+        return $this->belongsTo(DatosGeneralesEmbMa::class, 'DGEMMAid', 'id');
+    }
+}
