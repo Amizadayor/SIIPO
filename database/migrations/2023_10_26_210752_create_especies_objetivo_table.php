8b93bff710826bce4104836d5a2d7200cdda231d
Amizadayor <Juanamizadayo@gmail.com>
2023-10-26 15:10:12 -0600

    Controlador-Modelo-Migración 0.0.63

---
+++ database/migrations/2023_10_26_210752_create_especies_objetivo_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('especies_objetivo', function (Blueprint $table) {
+            $table->id();
+            $table->string('NombreComun', 50);
+            $table->string('NombreCientifico', 100);
+            $table->unsignedBigInteger('TPEspecieid');
+
+            $table->foreign('TPEspecieid')->references('id')->on('especies');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('especies_objetivo');
+    }
+};
