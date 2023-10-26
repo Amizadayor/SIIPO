2ecd179784dae8a677b60beaaf8b60330047493f
Amizadayor <Juanamizadayo@gmail.com>
2023-10-26 09:52:59 -0600

    Controlador-Modelo-Migración 0.0.27

---
+++ app/Models/MaterialCasco.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MaterialCasco extends Model
+{
+    use HasFactory;
+
+    protected $table = 'materiales_casco';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'NombreMaterialCasco'
+    ];
+    public $timestamps = true;
+}
