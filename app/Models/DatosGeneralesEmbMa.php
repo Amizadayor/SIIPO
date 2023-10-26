ff689f6a58d9b798c613b36e7de0191f5ee3c8c8
Amizadayor <Juanamizadayo@gmail.com>
2023-10-26 09:09:57 -0600

    Controlador-Modelo-Migración 0.0.23

---
+++ app/Models/DatosGeneralesEmbMa.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DatosGeneralesEmbMa extends Model
+{
+    use HasFactory;
+    protected $table = 'datosgenerales_emb_ma';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'NombreEmbMayor',
+        'RNPA',
+        'Matricula',
+        'PuertoBase',
+        'DocAcreditacionLegalMotor',
+        'DocCertificadoMatricula',
+        'DocComprobanteTenenciaLegal',
+        'DocCertificadoSegEmbs',
+        'UEEMMAid',
+    ];
+    public $timestamps = true;
+    public function UnidadEconomicaEmbMa()
+    {
+        return $this->belongsTo(UnidadEconomicaEmbMa::class, 'UEEMMAid', 'id');
+    }
+}
