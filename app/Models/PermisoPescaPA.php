ac15441508e6cdc326d59d6905b691a2a6648528
Amizadayor <Juanamizadayo@gmail.com>
2023-10-26 08:00:33 -0600

    Controlador-Modelo-Migración 0.0.14

--- app/Models/PermisoPescaPA.php
+++ app/Models/PermisoPescaPA.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\DetallePA;
+use App\Models\DatosGeneralesPA;
+use App\Models\PermisoPesca;
 
 class PermisoPescaPA extends Model
 {
