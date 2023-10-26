b14ce77777735f7f4105cf330f79883b161f11de
Amizadayor <Juanamizadayo@gmail.com>
2023-10-26 11:26:39 -0600

    Controlador-Modelo-Migración 0.0.45

---
+++ database/migrations/2023_10_26_172402_create_caractrsfis_emb_ma_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('caractrsfis_emb_ma', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('Eslora', 10, 2)->default(0.00);
+            $table->decimal('Manga', 10, 2)->default(0.00);
+            $table->decimal('Puntal', 10, 2)->default(0.00);
+            $table->decimal('Calado', 10, 2)->default(0.00);
+            $table->decimal('ArqueoNeto', 10, 2)->default(0.00);
+            $table->unsignedBigInteger('DGEMMAid');
+
+            $table->foreign('DGEMMAid')->references('id')->on('datosgenerales_emb_ma');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('caractrsfis_emb_ma');
+    }
+};
