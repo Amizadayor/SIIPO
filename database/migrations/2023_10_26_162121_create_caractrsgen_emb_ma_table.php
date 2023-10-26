f604ff608e77b1094d0fed8c1f0b672abfc92be7
Amizadayor <Juanamizadayo@gmail.com>
2023-10-26 10:25:30 -0600

    Controlador-Modelo-Migración 0.0.35

---
+++ database/migrations/2023_10_26_162121_create_caractrsgen_emb_ma_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('caractrsgen_emb_ma', function (Blueprint $table) {
+            $table->id();
+            $table->integer('CdPatrones');
+            $table->integer('CdMotoristas');
+            $table->integer('CdPSEspecializados');
+            $table->integer('CdPescadores');
+            $table->integer('AnioConstruccion');
+            $table->unsignedBigInteger('TPActid');
+            $table->unsignedBigInteger('TPCubid');
+            $table->unsignedBigInteger('MtrlCascoid');
+            $table->unsignedBigInteger('DGEMMAid');
+
+            $table->foreign('TPActid')->references('id')->on('tipos_actividad');
+            $table->foreign('TPCubid')->references('id')->on('tipos_cubierta');
+            $table->foreign('MtrlCascoid')->references('id')->on('materiales_casco');
+            $table->foreign('DGEMMAid')->references('id')->on('datosgenerales_emb_ma');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('caractrsgen_emb_ma');
+    }
+};
