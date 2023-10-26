2f99f626ffcb944bd0cc3f35d9781449acbb622b
Amizadayor <Juanamizadayo@gmail.com>
2023-10-26 10:29:35 -0600

    Controlador-Modelo-Migración 0.0.36

---
+++ database/migrations/2023_10_26_162717_create_artes_pesca_emb_ma_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('artes_pesca_emb_ma', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('TPArtPescaid');
+            $table->unsignedBigInteger('DGEMMAid');
+
+            $table->foreign('TPArtPescaid')->references('id')->on('artes_pesca');
+            $table->foreign('DGEMMAid')->references('id')->on('datosgenerales_emb_ma');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('artes_pesca_emb_ma');
+    }
+};
