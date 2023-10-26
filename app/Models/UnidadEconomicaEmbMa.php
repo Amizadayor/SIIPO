f65672f9791fb2efe4ccc19d3a4bc7e35214a3b2
Amizadayor <Juanamizadayo@gmail.com>
2023-10-26 09:02:00 -0600

    Controlador-Modelo-Migración 0.0.22

---
+++ app/Models/UnidadEconomicaEmbMa.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\UnidadEconomicaPA;
+
+class UnidadEconomicaEmbMa extends Model
+{
+    use HasFactory;
+    protected $table = 'unidadeconomica_emb_ma';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'RNPA',
+        'Nombre',
+        'ActivoPropio',
+        'UEDuenoid',
+    ];
+
+    public $timestamps = true;
+
+    public function UnidadEconomicaPA()
+    {
+        return $this->belongsTo(UnidadEconomicaPA::class, 'UEDuenoid', 'id');
+    }
+}
---
+++ database/migrations/2023_10_26_145623_create_unidadeconomica_emb_ma_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('unidadeconomica_emb_ma', function (Blueprint $table) {
+            $table->id();
+            $table->string('RNPA', 50)->nullable();
+            $table->string('Nombre', 50);
+            $table->boolean('ActivoPropio');
+            $table->unsignedBigInteger('UEDuenoid');
+
+            $table->foreign('UEDuenoid')->references('id')->on('unidadeconomica_pa');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('unidadeconomica_emb_ma');
+    }
+};
