c306ca548b407234ec8afb9783a49cb276a1cfca
Amizadayor <Juanamizadayo@gmail.com>
2023-10-26 15:45:44 -0600

    Controlador-Modelo-Migración 0.0.72

---
+++ app/Models/TipoProceso.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TipoProceso extends Model
+{
+    use HasFactory;
+    protected $table = 'tipos_proceso';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'NombreTipoProceso'
+    ];
+    public $timestamps = true;
+}
