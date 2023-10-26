ab1985bbd7f62b3696f60bdc3354cc5d3745bfd3
Amizadayor <Juanamizadayo@gmail.com>
2023-10-26 15:37:24 -0600

    Controlador-Modelo-Migración 0.0.69

---
+++ app/Http/Controllers/AdministracionTrabajadorIAController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AdministracionTrabajadorIA;
+use Illuminate\Http\Request;
+
+class AdministracionTrabajadorIAController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(AdministracionTrabajadorIA $administracionTrabajadorIA)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, AdministracionTrabajadorIA $administracionTrabajadorIA)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(AdministracionTrabajadorIA $administracionTrabajadorIA)
+    {
+        //
+    }
+}
