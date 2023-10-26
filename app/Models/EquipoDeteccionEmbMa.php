99367320681f4de78741d390ade1579ae2ec209d
Amizadayor <Juanamizadayo@gmail.com>
2023-10-26 10:36:00 -0600

    Controlador-Modelo-Migración 0.0.37

---
+++ app/Models/EquipoDeteccionEmbMa.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\EquipoDeteccion;
+use App\Models\DatosGeneralesEmbMa;
+
+class EquipoDeteccionEmbMa extends Model
+{
+    use HasFactory;
+    protected $table = 'equipos_deteccion_emb_ma';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'EqpoDeteccionid',
+        'DGEMMAid',
+    ];
+    public $timestamps = true;
+
+    public function EquipoDeteccion()
+    {
+        return $this->belongsTo(EquipoDeteccion::class, 'EqpoDeteccionid', 'id');
+    }
+
+    public function DatosGeneralesEmbMa()
+    {
+        return $this->belongsTo(DatosGeneralesEmbMa::class, 'DGEMMAid', 'id');
+    }
+}
