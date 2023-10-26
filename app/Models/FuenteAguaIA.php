b0fce45dc9dd39aeb33fb0e91039844cf2ec0b57
Amizadayor <Juanamizadayo@gmail.com>
2023-10-26 15:25:02 -0600

    Controlador-Modelo-Migración 0.0.67

---
+++ app/Models/FuenteAguaIA.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\DatosGeneralesIA;
+
+class FuenteAguaIA extends Model
+{
+    use HasFactory;
+    protected $table = 'fuentes_agua_ia';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'FTPozoProfundo',
+        'FTPozoCieloAbierto',
+        'FTRio',
+        'FTLago',
+        'FTArroyo',
+        'FTPresa',
+        'FTMar',
+        'FTOtro',
+        'FlujoAguaLxM',
+        'DGIAid',
+    ];
+    public $timestamps = true;
+    public function DatosGeneralesIA()
+    {
+        return $this->belongsTo(DatosGeneralesIA::class, 'DGIAid', 'id');
+    }
+}
