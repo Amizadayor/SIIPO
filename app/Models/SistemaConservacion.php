67d279590576fd9a336760944b87b13cba1fe6fd
Amizadayor <Juanamizadayo@gmail.com>
2023-10-26 09:58:12 -0600

    Controlador-Modelo-Migración 0.0.29

---
+++ app/Models/SistemaConservacion.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SistemaConservacion extends Model
+{
+    use HasFactory;
+    protected $table = 'sistemas_conservacion';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'NombreSistemaConservacion'
+    ];
+    public $timestamps = true;
+}
