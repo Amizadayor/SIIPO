d3fc87ef4ca93bd06b3c5c9bbc5dd5e35644c035
Amizadayor <Juanamizadayo@gmail.com>
2023-10-26 09:55:20 -0600

    Controlador-Modelo-Migración 0.0.28

---
+++ app/Models/EquipoDeteccion.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class EquipoDeteccion extends Model
+{
+    use HasFactory;
+    protected $table = 'equipos_deteccion';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'NombreEquipoDeteccion'
+    ];
+    public $timestamps = true;
+}
