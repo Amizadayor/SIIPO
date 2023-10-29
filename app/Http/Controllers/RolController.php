9adc8d48f02553d2e281c00384ed02c730e96fb2
Amizadayor <Juanamizadayo@gmail.com>
2023-10-29 17:36:11 -0600

    Actualización de Controlador - 0.0.01

--- app/Http/Controllers/RolController.php
+++ app/Http/Controllers/RolController.php
@@ -2,48 +2,89 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use App\Models\Rol;
+use App\Http\Responses\ApiResponse;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class RolController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Función para obtener la lista de roles
      */
     public function index()
     {
-        //
+        try {
+            $roles = Rol::all();
+        return ApiResponse::success('Lista de roles', 200, $roles);
+        } catch (Exception $e) {
+            return ApiResponse::error('Error al obtener la lista de roles: ' . $e->getMessage(), 500);
+        }
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Función para crear un nuevo rol
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $request->validate([
+                'NombreRol' => 'required|unique:roles',
+            ]);
+            $rol = Rol::create($request->all());
+        return ApiResponse::success('Rol creado exitosamente', 201, $rol);
+        } catch (ValidationException $e) {
+            return ApiResponse::error('Error de validacion: ' . $e->getMessage(), 422);
+        }
     }
 
     /**
-     * Display the specified resource.
+     * Función para obtener un rol por su id
      */
-    public function show(Rol $rol)
+    public function show(string $id)
     {
-        //
+        try {
+            $rol = Rol::findOrFail($id);
+        return ApiResponse::success('Rol obtenido exitosamente', 200, $rol);
+        } catch (ModelNotFoundException $e) {
+            return ApiResponse::error('Rol no encontrado', 404);
+        }
     }
 
     /**
-     * Update the specified resource in storage.
+     * Función para actualizar un rol
      */
-    public function update(Request $request, Rol $rol)
+    public function update(Request $request, $id)
     {
-        //
+        try {
+            $rol = Rol::findOrFail($id);
+            $request->validate([
+                'NombreRol' => 'required|unique:roles,NombreRol,' . $id
+            ]);
+            $rol->update($request->all());
+        return ApiResponse::success('Rol actualizado exitosamente', 200, $rol);
+        } catch (ModelNotFoundException $e) {
+            return ApiResponse::error('Rol no encontrado', 404);
+        } catch (Exception $e) {
+            return ApiResponse::error('Error: ' . $e->getMessage(), 422);
+        }
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Función para eliminar un rol
      */
-    public function destroy(Rol $rol)
+    public function destroy($id)
     {
-        //
+        try {
+            $rol = Rol::findOrFail($id);
+            $rol->delete();
+        return ApiResponse::success('Rol eliminado exitosamente', 200);
+        } catch (ModelNotFoundException $e) {
+            return ApiResponse::error('Rol no encontrado', 404);
+        } catch (Exception $e) {
+            return ApiResponse::error('Error: ' . $e->getMessage(), 422);
+        }
     }
 }
