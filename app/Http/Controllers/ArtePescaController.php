d5ed455e6c8618ecf576a328492e55107e7aaeab
Amizadayor <Juanamizadayo@gmail.com>
2023-11-04 00:32:36 -0600

    Actualización de Controlador - 0.0.15

--- app/Http/Controllers/ArtePescaController.php
+++ app/Http/Controllers/ArtePescaController.php
@@ -3,47 +3,100 @@
 namespace App\Http\Controllers;
 
 use App\Models\ArtePesca;
+use App\Http\Responses\ApiResponse;
 use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Validation\ValidationException;
+use Exception;
 
 class ArtePescaController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Obtiene la lista de los tipos de artes de pesca.
      */
     public function index()
     {
-        //
+        try {
+            $artespesca = ArtePesca::all();
+            return ApiResponse::success('Lista de los tipos de artes de pesca', 200, $artespesca);
+        } catch (Exception $e) {
+            return ApiResponse::error('Error al obtener la lista de artes de pesca: ' . $e->getMessage(), 500);
+        }
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Crea un nuevo tipo de arte de pesca.
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $request->validate([
+                'NombreArtePesca' => 'required|string|unique:artes_pesca'
+            ]);
+
+            $artepesca = ArtePesca::create($request->all());
+            return ApiResponse::success('Arte de pesca creado exitosamente', 201, $artepesca);
+        } catch (ValidationException $e) {
+            return ApiResponse::error('El arte de pesca ya existe: ' . $e->getMessage(), 422);
+        } catch (Exception $e) {
+            return ApiResponse::error('Error: ' . $e->getMessage(), 500);
+        }
     }
 
     /**
-     * Display the specified resource.
+     * Obtiene un tipo de arte de pesca por su id.
      */
-    public function show(ArtePesca $artePesca)
+    public function show($id)
     {
-        //
+        try{
+            $artepesca = ArtePesca::findOrFail($id);
+            return ApiResponse::success('Arte de pesca obtenido exitosamente', 200, $artepesca);
+        }catch(ModelNotFoundException $e){
+            return ApiResponse::error('Arte de pesca no encontrado',404);
+        } catch (Exception $e) {
+            return ApiResponse::error('Error: ' . $e->getMessage(), 500);
+        }
     }
 
     /**
-     * Update the specified resource in storage.
+     * Actualiza un tipo de arte de pesca por su id.
      */
-    public function update(Request $request, ArtePesca $artePesca)
+    public function update(Request $request, $id)
     {
-        //
+        try {
+            $artepesca = ArtePesca::findOrFail($id);
+            $request->validate([
+                'NombreArtePesca' => 'required|unique:artes_pesca,NombreArtePesca,' . $id,
+            ]);
+
+            $existeArtePesca = ArtePesca::where('NombreArtePesca', $request->NombreArtePesca)->first();
+            if ($existeArtePesca) {
+                return ApiResponse::error('El arte de pesca ya existe', 422);
+            }
+
+            $artepesca->update($request->all());
+            return ApiResponse::success('Arte de pesca actualizado exitosamente', 200, $artepesca);
+        } catch (ModelNotFoundException $e) {
+            return ApiResponse::error('Arte de pesca no encontrado', 404);
+        } catch (Exception $e) {
+            return ApiResponse::error('Error: ' . $e->getMessage(), 500);
+        }
     }
 
+
     /**
-     * Remove the specified resource from storage.
+     * función que elimina un tipo de arte de pesca por su id.
      */
-    public function destroy(ArtePesca $artePesca)
+    public function destroy($id)
     {
-        //
+        try {
+            $artepesca = ArtePesca::findOrFail($id);
+            $artepesca->delete();
+            return ApiResponse::success('Arte de pesca eliminado exitosamente', 200);
+        } catch (ModelNotFoundException $e) {
+            return ApiResponse::error('Arte de pesca no encontrado',404);
+        } catch (Exception $e) {
+            return ApiResponse::error('Error: ' . $e->getMessage(), 500);
+        }
     }
 }
