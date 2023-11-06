1a47d69d1ac2a73d789c3bf26004b896c67c8e11
Amizadayor <Juanamizadayo@gmail.com>
2023-11-05 23:51:35 -0600

    Actualización de Controlador - 0.0.09

--- app/Http/Controllers/OficinaController.php
+++ app/Http/Controllers/OficinaController.php
@@ -3,47 +3,137 @@
 namespace App\Http\Controllers;
 
 use App\Models\Oficina;
+use App\Models\Localidad;
+use App\Http\Responses\ApiResponse;
 use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Validation\ValidationException;
+use Exception;
 
 class OficinaController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Función que obtener todos las oficinas.
      */
     public function index()
     {
-        //
+        try {
+            $oficinas = Oficina::all();
+            $result = $oficinas->map(function ($item) {
+                return [
+                    'id' => $item->id,
+                    'NombreOficina' => $item->NombreOficina,
+                    'Ubicacion' => $item->Ubicacion,
+                    'Telefono' => $item->Telefono,
+                    'Email' => $item->Email,
+                    'created_at' => $item->created_at,
+                    'updated_at' => $item->updated_at,
+                ];
+            });
+            return ApiResponse::success('Lista de oficinas', 200, $result);
+        } catch (Exception $e) {
+            return ApiResponse::error('Error al obtener la lista de oficinas: ' . $e->getMessage(), 500);
+        }
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Función para crear una nueva Oficina
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $request->validate([
+                'NombreOficina' => 'required|string|max:50',
+                'Ubicacion' => 'required|string|max:100',
+                'Telefono' => 'required|string|max:10',
+                'Email' => 'required|string|max:40'
+            ]);
+
+            $existeOficina = Oficina::where('NombreOficina', $request->NombreOficina)
+                ->where('Ubicacion', $request->Ubicacion)
+                ->first();
+            if ($existeOficina) {
+                return ApiResponse::error('La oficina ya existe', 422);
+            }
+
+            $oficina = Oficina::create($request->all());
+            return ApiResponse::success('Oficina creada exitosamente', 201, $oficina);
+        } catch (ValidationException $e) {
+            return ApiResponse::error('Error de validación: ' . $e->getMessage(), 422, $e->errors());
+        } catch (Exception $e) {
+            return ApiResponse::error('Error al crear la oficina: ' . $e->getMessage(), 500);
+        }
     }
 
     /**
-     * Display the specified resource.
+     * Función que obtiene una oficina por su id.
      */
-    public function show(Oficina $oficina)
+    public function show($id)
     {
-        //
+        try {
+            $oficina = Oficina::findOrFail($id);
+            $result = [
+                'id' => $oficina->id,
+                'NombreOficina' => $oficina->NombreOficina,
+                'Ubicacion' => $oficina->Ubicacion,
+                'Telefono' => $oficina->Telefono,
+                'Email' => $oficina->Email,
+                'created_at' => $oficina->created_at,
+                'updated_at' => $oficina->updated_at,
+            ];
+            return ApiResponse::success('Oficina obtenida exitosamente', 200, $result);
+        } catch (ModelNotFoundException $e) {
+            return ApiResponse::error('Oficina no encontrada', 404);
+        } catch (Exception $e) {
+            return ApiResponse::error('Error al obtener la oficina: ' . $e->getMessage(), 500);
+        }
     }
 
     /**
-     * Update the specified resource in storage.
+     * Función para actualizar una oficina.
      */
-    public function update(Request $request, Oficina $oficina)
+    public function update(Request $request, $id)
     {
-        //
+        try {
+            $request->validate([
+                'NombreOficina' => 'required|string|max:50',
+                'Ubicacion' => 'required|string|max:100',
+                'Telefono' => 'required|string|max:10',
+                'Email' => 'required|string|max:40'
+            ]);
+
+            $existeOficina = Oficina::where('NombreOficina', $request->NombreOficina)
+                ->where('Ubicacion', $request->Ubicacion)
+                ->first();
+            if ($existeOficina) {
+                return ApiResponse::error('La oficina ya existe', 422);
+            }
+
+            $oficina = Oficina::findOrFail($id);
+            $oficina->update($request->all());
+            return ApiResponse::success('Oficina actualizada exitosamente', 200, $oficina);
+        } catch (ModelNotFoundException $e) {
+            return ApiResponse::error('Oficina no encontrada', 404);
+        } catch (ValidationException $e) {
+            return ApiResponse::error('Error de validación: ' . $e->getMessage(), 422, $e->errors());
+        } catch (Exception $e) {
+            return ApiResponse::error('Error al actualizar la oficina: ' . $e->getMessage(), 500);
+        }
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Función para eliminar una oficina.
      */
-    public function destroy(Oficina $oficina)
+    public function destroy($id)
     {
-        //
+        try {
+            $oficina = Oficina::findOrFail($id);
+            $oficina->delete();
+            return ApiResponse::success('Oficina eliminada exitosamente', 200);
+        } catch (ModelNotFoundException $e) {
+            return ApiResponse::error('Oficina no encontrada', 404);
+        } catch (Exception $e) {
+            return ApiResponse::error('Error al eliminar la oficina: ' . $e->getMessage(), 500);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\RegionController;
 use App\Http\Controllers\DistritoController;
 use App\Http\Controllers\MunicipioController;
 use App\Http\Controllers\LocalidadController;
+use App\Http\Controllers\OficinaController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
@@ -36,3 +37,4 @@ Route::apiResource('regiones', RegionController::class);
 Route::apiResource('distritos', DistritoController::class);
 Route::apiResource('municipios', MunicipioController::class);
 Route::apiResource('localidades', LocalidadController::class);
+Route::apiResource('oficinas', OficinaController::class);
